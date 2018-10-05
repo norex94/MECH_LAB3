69c6400973dccd6c91687500e6526ea765b2ac30
Arnór <arnoreids@gmail.com>
2018-10-05 20:25:22 +0000

    Setti inn GPS kóðann úr GPSTest

--- Logger.ino
+++ Logger.ino
@@ -160,15 +160,50 @@ bool logToSDCard(const char *name) {
 
 }
 
+void printToSerial() {
+	
+	Serial.print("\nTime: ");
+	Serial.print(GPS.hour, DEC); Serial.print(':');
+	Serial.print(GPS.minute, DEC); Serial.print(':');
+	Serial.print(GPS.seconds, DEC); Serial.print('.');
+	Serial.println(GPS.milliseconds);
+	Serial.print("Date: ");
+	Serial.print(GPS.day, DEC); Serial.print('/');
+	Serial.print(GPS.month, DEC); Serial.print("/20");
+	Serial.println(GPS.year, DEC);
+	Serial.print("Fix: "); Serial.print((int)GPS.fix);
+	Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+	if (GPS.fix) {
+		Serial.print("Location: ");
+		Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+		Serial.print(", ");
+		Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+		Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+		Serial.print("Angle: "); Serial.println(GPS.angle);
+		Serial.print("Altitude: "); Serial.println(GPS.altitude);
+		Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+	}
+
+
+}
+
 void loop()
 {
 
-	updateWheater();
 	readGPS();
+	// if millis() or timer wraps around, we'll just reset it
+	if (timer > millis()) timer = millis();
 
-	logToSDCard("Loggari.txt");
+	// approximately every 2 seconds or so, print out the current stats
+	if (millis() - timer > 1000) {
+		timer = millis(); // reset the timer
+		updateWheater();
+		logToSDCard("Loggari.txt");
+		printToSerial();
+		Serial.println("_________________________________");
 
-	
+	}
+
 
 
 	digitalWrite(PIN_A17, HIGH);
@@ -177,9 +212,8 @@ void loop()
 	
 
 
-	Serial.println("_________________________________");
 	
-	delay(1000);
+
 }
 
 
